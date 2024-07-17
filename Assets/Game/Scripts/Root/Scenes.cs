bceba70aaa011e238c2b2df0ccaab590d8fcf95c
Astar0th <astar0th-9@yandex.ru>
2024-07-17 14:49:13 +0700

    Initial project

---
+++ Assets/Game/Scripts/Root/Scenes.cs
@@ -0,0 +1,10 @@
+﻿namespace Game.Scripts.Root
+{
+    public static class Scenes
+    {
+        public const string BOOT = "Boot";
+        public const string MENU = "Menu";
+        public const string GAME_FIRST = "GameFirst";
+        public const string GAME_SECOND = "GameSecond";
+    }
+}
---
+++ Assets/Game/Scripts/Utils/Coroutines.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Game.Scripts.Utils
+{
+    public class Coroutines : MonoBehaviour
+    {
+    
+    }
+}
